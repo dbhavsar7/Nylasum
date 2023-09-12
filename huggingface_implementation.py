4180dbae256073c694fde986c0cdeed3958fb27f
AjeetParmar1 <ajeetparmar45@gmail.com>
2023-09-12 12:35:19 -0400

    huggingface implementation w/ token

---
+++ huggingface_implementation.py
@@ -0,0 +1,10 @@
+from huggingface_hub import notebook_login
+
+# I made a fake huggingface account for testing purposes: 
+#  username: RoySchneider316
+#  email: ceges15395@tenjb.com
+#  password: Albany518!?!
+# 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
+
+notebook_login(target= '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')
+# notebook_login('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')
